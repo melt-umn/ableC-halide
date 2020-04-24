4c16aa7e1a3c994c860270c434e20210cddc2151
Lucas Kramer <krame505@umn.edu>
2020-04-23 19:57:58 -0500

    Use host versions of operators in pattern matching for transformation to IterStmt representation

--- grammars/edu.umn.cs.melt.exts.ableC.halide/abstractsyntax/IterStmt.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.halide/abstractsyntax/IterStmt.sv
@@ -181,7 +181,7 @@ IterStmt ::= s::Decorated Stmt
     | ableC_Stmt { if ($Expr{c}) $Stmt{t} else $Stmt{e} } ->
       condIterStmt(c, stmtToIterStmt(t), stmtToIterStmt(e))
     | ableC_Stmt {
-        for ($BaseTypeExpr{t} $Name{i1} = 0; $Name{i2} < $Expr{n}; $Name{i3}++)
+        for ($BaseTypeExpr{t} $Name{i1} = 0; $Name{i2} host::< $Expr{n}; $Name{i3} host::++)
           $Stmt{b}
       } when i1.name == i2.name && i1.name == i3.name ->
       forIterStmt(t, baseTypeExpr(), i1, n, stmtToIterStmt(b))
